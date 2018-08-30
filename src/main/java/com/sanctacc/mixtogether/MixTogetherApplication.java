418ac85cb48f623d5e89354f6529aec16ee623fa
SanctaCC <mraxeo@gmail.com>
2018-08-30 20:31:54 +0200

    spring boot init

---
+++ src/main/java/com/sanctacc/mixtogether/MixTogetherApplication.java
@@ -0,0 +1,12 @@
+package com.sanctacc.mixtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixTogetherApplication.class, args);
+	}
+}
