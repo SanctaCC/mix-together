3eb61ae7a9804efe1705c0a4029590f5287868ba
SanctaCC <mraxeo@gmail.com>
2018-09-09 01:03:29 +0200

    swagger-ui

--- src/main/java/com/sanctacc/mixtogether/MixTogetherApplication.java
+++ src/main/java/com/sanctacc/mixtogether/MixTogetherApplication.java
@@ -9,15 +9,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableSwagger2
+@Configuration
 public class MixTogetherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MixTogetherApplication.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select().apis(RequestHandlerSelectors.any()).build();
 	}
 
 	@Bean
