fd9421a259234dc33dcdadd85a2c38dd6b375970
hdolny <mraxeo@gmail.com>
2018-09-13 13:17:42 +0200

    play next videos automatically

--- app/src/views/Player.vue
+++ app/src/views/Player.vue
@@ -1,22 +1,49 @@
 <template>
-  <div>
-    <youtube :video-id="videoId" :player-vars="{ autoplay: 1 }"></youtube>
-    <button @click.prevent="change">Change</button>
-  </div>
+    <div>
+        <youtube :video-id="videoId" @ready="ready" @ended="ended" @playing="playing" @paused="paused"
+                 :player-vars="{ autoplay: 1 }"></youtube>
+        <button @click.prevent="previous">Previous</button>
+        <button @click.prevent="next">Next</button>
+        <br>video id: {{videoId}}
+        <br/> queue: {{ids}}
+    </div>
 </template>
 
 <script>
-export default {
-  name: "player",
-  data() {
-    return {
-      videoId: "t_6sbEEIvjI"
+    let i = 0;
+    export default {
+        name: "player",
+        data() {
+            return {
+                videoId: "",
+                ids: ["cdwal5Kw3Fc", "t_6sbEEIvjI", "RHg53wMflCc"]
+            }
+        },
+        methods: {
+            change() {
+                this.videoId = this.ids[Math.abs(Math.abs(i % this.ids.length))];
+            },
+            previous() {
+                i--;
+                this.change();
+            },
+            next() {
+                i++;
+                this.change();
+            },
+            ended(event) {
+                console.log(event);
+                this.next();
+            },
+            playing(event) {
+                console.log(event);
+            },
+            paused(event) {
+                console.log(event);
+            },
+            ready(event) {
+                this.change()
+            }
+        }
     }
-  },
-  methods: {
-    change() {
-      this.videoId = "i-daiA1RNp4"
-    }
-  }
-}
 </script>
